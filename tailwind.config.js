cfff4702a5688e71e8cea4d1b33808bd5e3fbdda
unknown <sithumin2312>
2023-09-08 23:26:06 +0630

    Guck Git

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
+      colors:{
+        'primary' : '#0bbbcc',
+      }
+    },
+  },
+  plugins: [],
+}
+
